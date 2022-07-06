f1c567d7ad90d04f38de29858330d1653ff309d7
koki sato <s17161278@st.daito.ac.jp>
2022-07-06 23:02:08 +0900

    keydown keyupのテスト中

--- resources/views/minigame/start.blade.php
+++ resources/views/minigame/start.blade.php
@@ -5,6 +5,38 @@
 
 @section('content')
 
-<h1>PLACEHOLDER</h1>
+<div id='message'></div>
+<div id='result'></div>
+
+@endsection
+
+
+@section('script')
+
+$(function(){
+    console.log('ok');
+    let start = null;
+    let stop = null;
+
+    $(window).keydown(function(e){
+        if(e.keyCode === 32){
+            start = new Date();
+            console.log(start);
+            $("#message").text('space button is keydown');
+        }
+    });
+
+    $(window).keyup(function(e){
+        if(e.keyCode === 32){
+            stop = new Date()
+            console.log(stop);
+
+            $("#message").text('space button is keyup');
+            $("#result").text(stop - start);
+
+        }
+    });
+
+})
 
 @endsection
